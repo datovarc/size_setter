85394248abd872330c22acb5da5f450bd37ef5aa
datovarc <33615845+datovarc@users.noreply.github.com>
2025-03-27 20:44:05 +0800

    Update size_setter.dart

--- lib/size_setter.dart
+++ lib/size_setter.dart
@@ -12,10 +12,11 @@ abstract class SizeSetterUtils {
     required BuildContext context,
     double sourceWidth = 400,
     double sourceHeight = 820,
+    double offset = 1,
   }) {
     /// Set Th value to common accessable variable
     Utils.setSizers(
-        context: context, setupWidth: sourceWidth, setupHeight: sourceHeight);
+        context: context, setupWidth: sourceWidth, setupHeight: sourceHeight, setupOffset: offset);
   }
 }
 
@@ -24,13 +25,17 @@ class SizeSetter extends StatelessWidget {
   const SizeSetter({
     super.key,
     required this.child,
+    
   });
   final Widget child;
+  final double sourceWidth;
+  final double sourceHeight;
+  final double sourceOffset;
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: ((context, constraints) {
       /// This method is to initialize source Device size
-      SizeSetterUtils.setSourceDeviceSize(context: context);
+      SizeSetterUtils.setSourceDeviceSize(context: context, sourceWidth: sourceWidth, sourceHeight: sourceHeight, sourceOffset: sourceOffset);
       return child;
     }));
   }
